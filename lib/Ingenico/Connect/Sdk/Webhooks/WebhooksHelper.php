d1979a0f2497f07675adb9fa038f393540ebe7c9
Stefan Doorn <stefan@efectos.nl>
2018-05-14 20:03:47 +0200

    Import WebhooksEvent class for return docblock

--- lib/Ingenico/Connect/Sdk/Webhooks/WebhooksHelper.php
+++ lib/Ingenico/Connect/Sdk/Webhooks/WebhooksHelper.php
@@ -3,6 +3,7 @@ namespace Ingenico\Connect\Sdk\Webhooks;
 
 use Ingenico\Connect\Sdk\Client;
 use Ingenico\Connect\Sdk\DefaultConnectionResponse;
+use Ingenico\Connect\Sdk\Domain\Webhooks\WebhooksEvent;
 use Ingenico\Connect\Sdk\ResponseClassMap;
 use Ingenico\Connect\Sdk\ResponseFactory;
 
