f2ed046e823468ee55da1a6b19bca25c8ec13746
Ingenico ePayments <github@epay.ingenico.com>
2022-10-06 09:42:14 +0200

    Release 6.33.0.

--- src/Ingenico/Connect/Sdk/Domain/Definitions/MicrosoftFraudResults.php
+++ src/Ingenico/Connect/Sdk/Domain/Definitions/MicrosoftFraudResults.php
@@ -14,9 +14,29 @@ use UnexpectedValueException;
 class MicrosoftFraudResults extends DataObject
 {
     /**
+     * @var string
+     */
+    public $deviceCountryCode = null;
+
+    /**
+     * @var string
+     */
+    public $deviceId = null;
+
+    /**
      * @var int
      */
     public $fraudScore = null;
+
+    /**
+     * @var string
+     */
+    public $trueIpAddress = null;
+
+    /**
+     * @var string
+     */
+    public $userDeviceType = null;
 
     /**
      * @return object
@@ -24,8 +44,20 @@ class MicrosoftFraudResults extends DataObject
     public function toObject()
     {
         $object = parent::toObject();
+        if (!is_null($this->deviceCountryCode)) {
+            $object->deviceCountryCode = $this->deviceCountryCode;
+        }
+        if (!is_null($this->deviceId)) {
+            $object->deviceId = $this->deviceId;
+        }
         if (!is_null($this->fraudScore)) {
             $object->fraudScore = $this->fraudScore;
+        }
+        if (!is_null($this->trueIpAddress)) {
+            $object->trueIpAddress = $this->trueIpAddress;
+        }
+        if (!is_null($this->userDeviceType)) {
+            $object->userDeviceType = $this->userDeviceType;
         }
         return $object;
     }
@@ -38,8 +70,20 @@ class MicrosoftFraudResults extends DataObject
     public function fromObject($object)
     {
         parent::fromObject($object);
+        if (property_exists($object, 'deviceCountryCode')) {
+            $this->deviceCountryCode = $object->deviceCountryCode;
+        }
+        if (property_exists($object, 'deviceId')) {
+            $this->deviceId = $object->deviceId;
+        }
         if (property_exists($object, 'fraudScore')) {
             $this->fraudScore = $object->fraudScore;
+        }
+        if (property_exists($object, 'trueIpAddress')) {
+            $this->trueIpAddress = $object->trueIpAddress;
+        }
+        if (property_exists($object, 'userDeviceType')) {
+            $this->userDeviceType = $object->userDeviceType;
         }
         return $this;
     }
